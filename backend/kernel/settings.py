d6684ddb201596ed84f2148e6078fbfccbb4b3a1
Luciano Martins <luciano.martins@quay.com.br>
2026-04-21 14:48:48 -0300

    add CORS_ALLOWED_ORIGINS

--- backend/kernel/settings.py
+++ backend/kernel/settings.py
@@ -56,6 +56,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:9000",
     "http://localhost:9000",
+    "http://ondance-frontend.ddns.net",
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
